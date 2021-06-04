70440119e3016a8a5e049e37a07c9ba709a820ee
pratph <85346223+pratph@users.noreply.github.com>
2021-06-04 17:31:10 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -1,13 +1,13 @@
 class config:
-    BOT_TOKEN = ""
-    APP_ID = ""
-    API_HASH = ""
-    DATABASE_URL = ""
-    SUDO_USERS = "" # Sepearted by space.
-    SUPPORT_CHAT_LINK = ""
+    BOT_TOKEN = "1855643474:AAEcTA36sDPspCscYlLhY9RckF_LvER53Pw"
+    APP_ID = "3739793"
+    API_HASH = "5d32b5c59ff3a7529cf22414b700269e"
+    DATABASE_URL = "postgres://uokqjylzuuhhbw:604cfb8938e094acfa2d3e9dfe63d848ee16db602ef4a9e2e08bde5f5e6347d4@ec2-35-169-188-58.compute-1.amazonaws.com:5432/dfimlfr3a0mnla"
+    SUDO_USERS = "1752576796 1432545229 1328943334 1722060868" # Sepearted by space.
+    SUPPORT_CHAT_LINK = "https://t.me/joinchat/46eF008p2Bg3NTUx"
     DOWNLOAD_DIRECTORY = "./downloads/"
-    G_DRIVE_CLIENT_ID = ""
-    G_DRIVE_CLIENT_SECRET = ""
+    G_DRIVE_CLIENT_ID = "114221263886-cscr4c0kpoatfo458lcllf6brpotfqp0.apps.googleusercontent.com"
+    G_DRIVE_CLIENT_SECRET = "gc6_cV9yrHovAe1hR5DAA0rw"
 
 
 class BotCommands:
