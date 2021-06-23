a23d96cd19448613eff6a099adba11bc88206f86
ynewm <86358389+ynewm@users.noreply.github.com>
2021-06-23 18:07:09 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -1,13 +1,13 @@
 class config:
-    BOT_TOKEN = "1855643474:AAEcTA36sDPspCscYlLhY9RckF_LvER53Pw"
-    APP_ID = "3739793"
-    API_HASH = "5d32b5c59ff3a7529cf22414b700269e"
-    DATABASE_URL = "postgres://uokqjylzuuhhbw:604cfb8938e094acfa2d3e9dfe63d848ee16db602ef4a9e2e08bde5f5e6347d4@ec2-35-169-188-58.compute-1.amazonaws.com:5432/dfimlfr3a0mnla"
-    SUDO_USERS = "1752576796 1432545229 1328943334 1722060868" # Sepearted by space.
+    BOT_TOKEN = "1739296333:AAGMTZp31N0KvhyacJgDz7M3Sm6eH9D1cR4"
+    APP_ID = "6369091"
+    API_HASH = "b569574b6a229d822c76a791736e4734"
+    DATABASE_URL = "postgres://xzvqrlqcnmwqbw:e976d053d5875f7a9b91b5b0ccfb3abf0ce566f71cca4c787854bf0ac998163a@ec2-35-169-188-58.compute-1.amazonaws.com:5432/d9lqkad0tocl6u"
+    SUDO_USERS = "1737765849" # Sepearted by space.
     SUPPORT_CHAT_LINK = "https://t.me/joinchat/46eF008p2Bg3NTUx"
     DOWNLOAD_DIRECTORY = "./downloads/"
-    G_DRIVE_CLIENT_ID = "114221263886-cscr4c0kpoatfo458lcllf6brpotfqp0.apps.googleusercontent.com"
-    G_DRIVE_CLIENT_SECRET = "gc6_cV9yrHovAe1hR5DAA0rw"
+    G_DRIVE_CLIENT_ID = "216115346945-2s5557pakot9dv4ejfafvaop75436jik.apps.googleusercontent.com"
+    G_DRIVE_CLIENT_SECRET = "mVsoxsCZZhId4-gjXFHkX4yR"
 
 
 class BotCommands:
